3cb0d99d2b97b9eaa57210451b4837aab45fa1d6
Borys1no <boryscereceda@hotmail.com>
2023-09-18 23:05:48 -0500

    Creacion de carpetas y del index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicio</title>
+</head>
+<body>
+<div class="brack">
+  <img src="/view/static/img/foto1.jpeg" alt="background" srcset="">
+</div>
+<div class="btnInicio">
+  <a href="/view/home.php">Iniciar</a>
+</div>
+</body>
+</html>
