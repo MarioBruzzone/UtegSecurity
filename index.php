f6603674d33ef9b1d15fadb3fa96b549329fa36b
Lembe <bruzzone123@gmail.com>
2023-09-23 17:09:03 -0500

    shit

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/view/static/css/inicio.css">
+  <link rel="stylesheet" href="./view/static/css/inicio.css">
   <title>Inicio</title>
 </head>
 <body>
 <div class="btnInicio">
-  <a href="/view/home.php">Iniciar</a>
+  <a href="./view/home.php">Iniciar</a>
 </div>
 </body>
 </html>
