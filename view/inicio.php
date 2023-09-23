f40445ff273a4939c5bdaa217bbb8a9ac588c68b
Borys1no <boryscereceda@hotmail.com>
2023-09-22 22:29:02 -0500

    pendiente validar sessions

--- view/inicio.php
+++ view/inicio.php
@@ -5,7 +5,7 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
 if(!isset($_SESSION["username"])){
-  header("Location: /view/inicio.php");
+  header("Location: ../controller/log_in.php");
 
   exit();
 }
